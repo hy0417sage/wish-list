f9bb068c3c802b1c396d57524f6ee6fc38593e6a
Song HyunYoung <97173983+hy0417sage@users.noreply.github.com>
2023-04-06 20:44:36 +0900

    feat: details 상세화면 추가

---
+++ app/src/main/java/com/hy0417sage/wishlist/ui/views/home/details/DetailsActivity.kt
@@ -0,0 +1,28 @@
+package com.hy0417sage.wishlist.ui.views.home.details
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.hy0417sage.wishlist.data.model.WishEntity
+import com.hy0417sage.wishlist.databinding.ActivityDetailsBinding
+
+class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val data = if (Build.VERSION.SDK_INT >= 33) {
+            intent.getParcelableExtra("data", WishEntity::class.java)
+        } else {
+            intent.getParcelableExtra<WishEntity>("data")
+        }
+
+        Log.d("DetailsActivity", "$data")
+    }
+}
