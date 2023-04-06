bf928d31047f6140a9ae85dd356e99a4f983957c
Song HyunYoung <97173983+hy0417sage@users.noreply.github.com>
2023-04-06 20:45:10 +0900

    chore: 직렬화/역직렬화 데이터 전달을 위해 Parcelable 설정

--- app/src/main/java/com/hy0417sage/wishlist/data/model/WishEntity.kt
+++ app/src/main/java/com/hy0417sage/wishlist/data/model/WishEntity.kt
@@ -1,13 +1,16 @@
 package com.hy0417sage.wishlist.data.model
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @Entity(tableName = "wish_list")
 data class WishEntity(
     @PrimaryKey(autoGenerate = true) var id: Int? = null,
     @ColumnInfo(name = "title") val title: String?,
     @ColumnInfo(name = "image") val image: String?,
-    @ColumnInfo(name = "price") val price: String?
-)
+    @ColumnInfo(name = "price") val price: String?,
+) : Parcelable
