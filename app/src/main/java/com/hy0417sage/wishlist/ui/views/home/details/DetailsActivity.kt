c7c1b70073159d03b7908a315c90a56bce32a746
Song HyunYoung <97173983+hy0417sage@users.noreply.github.com>
2023-04-06 22:02:02 +0900

    feat: details 화면, 아이템 구매 링크 이동할 수 있는 버튼 추가

--- app/src/main/java/com/hy0417sage/wishlist/ui/views/home/details/DetailsActivity.kt
+++ app/src/main/java/com/hy0417sage/wishlist/ui/views/home/details/DetailsActivity.kt
@@ -1,5 +1,7 @@
 package com.hy0417sage.wishlist.ui.views.home.details
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -24,5 +26,9 @@ class DetailsActivity : AppCompatActivity() {
         }
 
         Log.d("DetailsActivity", "$data")
+
+        binding.button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(data?.url)))
+        }
     }
 }
